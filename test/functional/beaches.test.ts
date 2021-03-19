ad280cec37c64ba30fde3eed4af2923caf428ca2
Adriano Santos <adrianodrix@gmail.com>
2021-03-19 14:37:03 -0300

    test: ensure create a beach with success

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -1,46 +1,16 @@
-describe('Beach forescast funcional tests', () => {
-  it('should return a forecast with just a few times', async () => {
-    const { body, status } = await global.testRequest.get('/forecast');
-    expect(status).toBe(200);
-    expect(body).toEqual([
-      {
-        time: '2020-04-26T00:00:00+00:00',
-        forecast: [
-          {
-            lat: -33.792726,
-            lng: 151.289824,
-            name: 'Manly',
-            position: 'E',
-            rating: 2,
-            swellDirection: 64.26,
-            swellHeight: 0.15,
-            swellPeriod: 3.89,
-            time: '2020-04-26T00:00:00+00:00',
-            waveDirection: 231.38,
-            waveHeight: 0.47,
-            windDirection: 299.45,
-          },
-        ],
-      },
-      {
-        time: '2020-04-26T01:00:00+00:00',
-        forecast: [
-          {
-            lat: -33.792726,
-            lng: 151.289824,
-            name: 'Manly',
-            position: 'E',
-            rating: 2,
-            swellDirection: 123.41,
-            swellHeight: 0.21,
-            swellPeriod: 3.67,
-            time: '2020-04-26T01:00:00+00:00',
-            waveDirection: 232.12,
-            waveHeight: 0.46,
-            windDirection: 310.48,
-          },
-        ],
-      },
-    ]);
+describe('Beaches functional tests', () => {
+  describe('When creating a beach', () => {
+    it('should create a beach with success', async () => {
+      const newBeach = {
+        lat: -33.792726,
+        lng: 151.289824,
+        name: 'Manly',
+        position: 'E',
+      };
+
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+      expect(response.status).toBe(201);
+      expect(response.body).toEqual(expect.objectContaining(newBeach));
+    });
   });
 });
