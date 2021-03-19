99ab8fec6b41544bbf2099d503f25c92e2dcd0f1
Adriano Santos <adrianodrix@gmail.com>
2021-03-19 17:31:32 -0300

    refactor: remove code to a helper method

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -8,16 +8,24 @@ export abstract class BaseController {
     error: mongoose.Error.ValidationError | Error
   ): void {
     if (error instanceof mongoose.Error.ValidationError) {
-      const duplicatedKindErros = Object.values(error.errors).filter(
-        (err) => err.kind === CUSTOM_VALIDATION.DUPLICATED
-      );
-      if (duplicatedKindErros.length) {
-        res.status(409).send({ code: 409, error: error.message });
-      } else {
-        res.status(422).send({ code: 422, error: error.message });
-      }
+      const clientErrors = this.handleClientErrors(error);
+      res.status(clientErrors.code).send(clientErrors);
     } else {
       res.status(500).send({ code: 500, error: 'Something went wrong!' });
     }
+  }
+
+  private handleClientErrors(
+    error: mongoose.Error.ValidationError
+  ): { code: number; error: string } {
+    const duplicatedKindErros = Object.values(error.errors).filter(
+      (err) => err.kind === CUSTOM_VALIDATION.DUPLICATED
+    );
+
+    if (duplicatedKindErros.length) {
+      return { code: 409, error: error.message };
+    }
+
+    return { code: 422, error: error.message };
   }
 }
