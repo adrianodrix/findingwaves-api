bb0727dd853d3c0ad0c207ed73cb95dfc46cf6ba
Adriano Santos <adrianodrix@gmail.com>
2021-03-22 19:12:35 -0300

    feat: rate limit route /forecast

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -1,17 +1,42 @@
-import { ClassMiddleware, Controller, Get } from '@overnightjs/core';
+import {
+  ClassMiddleware,
+  Controller,
+  Get,
+  Middleware,
+} from '@overnightjs/core';
+import rateLimit from 'express-rate-limit';
+
 import logger from '@src/logger';
 import { authMiddleware } from '@src/middlewares/auth';
 import { Beach } from '@src/models/beach';
 import { Forecast } from '@src/services/forecast';
 import { Request, Response } from 'express';
 import { BaseController } from '.';
+import ApiError from '@src/utils/errors/api-error';
 
 const forecast = new Forecast();
+
+const rateLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute in milliseconds
+  max: 2,
+  keyGenerator(req: Request): string {
+    return req.ip;
+  },
+  handler(_, res: Response): void {
+    res.status(429).send(
+      ApiError.format({
+        code: 429,
+        message: "Too many requests to the '/forecast endpoint'",
+      })
+    );
+  },
+});
 
 @Controller('forecast')
 @ClassMiddleware(authMiddleware)
 export class ForecastController extends BaseController {
   @Get('')
+  @Middleware(rateLimiter)
   public async getForecastForLoggedUser(
     req: Request,
     res: Response
